ea3efaa93d408fc023dc210553bd17c9c6b0c0b5
siddH17 <sid17052001@gmail.com>
2023-02-14 13:25:12 +0530

    l

---
+++ Main_Content/mathematics.js
@@ -0,0 +1,9 @@
+function openNav()  {
+    document.getElementById('chemsidebar').style.width = "250px";
+    document.getElementById('main').style.marginLeft = "250px";
+    document.getElementById('chemsidebar').style.height = "1500px";
+}
+function closeNav() {
+    document.getElementById('chemsidebar').style.width = "0";
+    document.getElementById('main').style.marginLeft = "0";
+}
---
+++ Main_Content/physics.js
@@ -0,0 +1,9 @@
+function openNav()  {
+    document.getElementById('physidebar').style.width = "250px";
+    document.getElementById('main').style.marginLeft = "250px";
+    document.getElementById('physidebar').style.height = "1000px";
+}
+function closeNav() {
+    document.getElementById('physidebar').style.width = "0";
+    document.getElementById('main').style.marginLeft = "0";
+}
