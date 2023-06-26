81462188781c8087f1462e17c3b36835588e77b5
siddH17 <sid17052001@gmail.com>
2023-06-26 21:29:23 +0530

    updated changes

--- Main_Content/physics.js
+++ Main_Content/physics.js
@@ -1,3 +1,17 @@
+// jquery code
+$(document).ready(function(){
+    $(".closebtn").hide();
+    $(".openbtn").click(function(){
+        $(".openbtn").hide();
+        $(".closebtn").show();
+    });
+    $(".closebtn").click(function(){
+        $(".closebtn").hide();
+        $(".openbtn").show();
+    });
+});
+
+// javascript code
 function openNav()  {
     document.getElementById('physidebar').style.width = "250px";
     document.getElementById('main').style.marginLeft = "250px";
