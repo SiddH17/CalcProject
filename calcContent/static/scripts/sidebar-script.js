6b0af3495b89c886fe20fcd7de6e8cea93565bd6
siddH17 <sid17052001@gmail.com>
2023-09-04 09:45:35 +0530

    updated changes on the calc

--- calcContent/static/scripts/sidebar-script.js
+++ calcContent/static/scripts/sidebar-script.js
@@ -16,8 +16,19 @@ function openNav()  {
     document.getElementById('physidebar').style.width = "250px";
     document.getElementById('main').style.marginLeft = "250px";
     document.getElementById('physidebar').style.height = "100vh";
+    document.getElementById('phybody').style.marginLeft = '245px';
 }
 function closeNav() {
     document.getElementById('physidebar').style.width = "0";
     document.getElementById('main').style.marginLeft = "0";
+    document.getElementById('phybody').style.marginLeft = '0';
+}
+function openMathNav()  {
+    document.getElementById('mathsidebar').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('mathsidebar').style.height = '100vh';
+}
+function closeMathNav() {
+    document.getElementById('mathsidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
 }
